10c9f0a5fe61f00a3a7fba8549ef2625707f866b
Jefferson C. Sia <awf-jsia@zetaglobal.com>
2018-08-01 10:17:45 +0800

    added get onsite recommendations test

---
+++ locust_scripts/osr_get_recs.py
@@ -0,0 +1,23 @@
+from locust import HttpLocust, TaskSet, task
+import json
+import random
+from datetime import datetime
+
+
+class UserBehavior(TaskSet):
+
+    def on_start(self):
+        self.sites = ['qa-osr-site1', 'qa-osr-site-2', 'qa-osr-site3']
+        self.bsins = json.load(open("osr_data/bsins.json"))
+
+    @task
+    def get_onsite_recommendations(self):
+        r = self.client.get("/api/v1/osrs?site_id={}&bsin={}&selectors=%23selector1".format(random.choice(self.sites), random.choice(self.bsins)))
+        assert r.status_code is 200, "Unexpected response code: " + str(r.status_code)
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 2000
+    max_wait = 5000
+    host = 'https://onsiterecs.api.boomtrain.com'
